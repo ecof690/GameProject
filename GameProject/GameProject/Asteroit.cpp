a698dc0300e3ccf9bb817db08e31ddaa6d2bbdee
Ercan <52319063+ecof690@users.noreply.github.com>
2019-12-15 13:48:25 +0300

    Asteroit header and cpp created.

---
+++ GameProject/GameProject/Asteroit.cpp
@@ -0,0 +1,6 @@
+#include "Asteroit.h"
+
+Asteroit::Asteroit(){
+	health = 0;
+	speed = 0;
+}//End of default constructor.
---
+++ GameProject/GameProject/Asteroit.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Asteroit {
+protected:
+	int health;
+	double speed;
+public:
+	Asteroit();
+
+
+
+};
