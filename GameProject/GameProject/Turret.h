030ff9b93f8bc98183c0fa11d613fab15755aa2f
Tolga KALAYCIOĞLU <47540955+tolgak99@users.noreply.github.com>
2019-12-30 01:41:49 +0300

    Hit Asteroit
    Added  "Asteroit.h" class and hitAsteroit function to control spaceship hit the asteroits.

--- GameProject/GameProject/Turret.h
+++ GameProject/GameProject/Turret.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Gun.h"
+#include "Asteroit.h"
 
 class Turret :public Gun {
 
@@ -7,7 +8,7 @@ public:
 	Turret();
 	void fire(int, int, int);
 	int hitWall(int, int);
-
+        int  hitAsteroit(int , int ,int,Asteroit&);
 private:
 	int dist;
 
